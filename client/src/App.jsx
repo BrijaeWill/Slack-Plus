72b8a1baad0b340fe25d15971635e7831ccac68e
Louis Pogorelc <pogorelclouis@gmail.com>
2024-12-30 19:57:38 -0500

    Added majority of the functionality portions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 import Home from './pages/home';
+
+const socket = io.connect('http://localhost:4000');
 function App() {
+  const [username, Setusername] = useState('');
+  const [room, SetRoom] = useState('');
   return(
     <Router>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route 
+            path='/' 
+            element={
+              <Home
+                username={username}
+                serUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
